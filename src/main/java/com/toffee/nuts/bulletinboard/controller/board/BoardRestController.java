b02537dcf723181116a0226e8739912aabba1ab3
LeeTangle <adszzz11@naver.com>
2023-01-20 20:57:05 +0900

    board system init

--- src/main/java/com/toffee/nuts/bulletinboard/controller/board/BoardRestController.java
+++ src/main/java/com/toffee/nuts/bulletinboard/controller/board/BoardRestController.java
@@ -1,8 +1,10 @@
 package com.toffee.nuts.bulletinboard.controller.board;
 
 
+import com.toffee.nuts.bulletinboard.domain.Board;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class BoardRestController {
@@ -10,6 +12,7 @@ public class BoardRestController {
     @PostMapping("/board")
     public String board() {
         System.out.println("board init");
-        return "index.html";
+
+        return "ㅁㄴㅇㄹ";
     }
 }
